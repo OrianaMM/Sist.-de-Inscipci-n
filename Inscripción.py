e44bcd3c3d4413b4a13e15ecf3ebfd2fe9e1bb15
Oriana <ori_morcillo00@hotmail.com>
2026-06-26 21:18:34 -0300

    Añadí la validación de los cupos

--- Inscripción.py
+++ Inscripción.py
@@ -1,28 +1,51 @@
 import json
 
+def guardar_inscripcion(alumno, datos, arch):
+ datos.append(alumno)
+ with open(arch, "w", encoding="utf-8") as archivo: #guardado del alumno
+        json.dump(datos, archivo, indent=4, ensure_ascii=False)
+ print("¡Ya estás inscripto!")  
+
+def obtenerInscriptosValidacion(curso, datos):
+  cupos= 25
+  inscriptos_curso= 0
+  for d in datos:
+      if d.get("Nombre de Curso/Taller") == curso: #busca cupos en los curso
+         inscriptos_curso = inscriptos_curso + 1    
+ 
+  if inscriptos_curso >= cupos:
+    print(f"\n Lo sentimos, no quedan más lugares para el {curso}")
+    inscriptos_curso -= 1
+    return -1 #esto indica que el curso está lleno
+  return inscriptos_curso
+      
+
 def inscripcion(curso):
+    arch= "inscriptos.json"
+    try:
+        with open(arch,"r", encoding="utf-8") as archivo: #apertura del archivo en esc/lec
+            datos= json.load(archivo) #cargo datos
+    except (FileNotFoundError, json.JSONDecodeError):
+             datos=[] #empiezo una lista nueva 
+
+    incriptosActu= obtenerInscriptosValidacion(curso, datos)
+    if incriptosActu == -1: #indica que esta lleno y detiene la fn
+     return 
+    
     print("\n")
     print("Está inscribiendose al ", curso)
     nombre= str(input("Indique sus Nombres sin el apellido: "))
     apellido= str(input("Indique su Apellido: "))
     conocimiento= str(input("Indique su conocimiento sobre la materia (Alto, Medio Nulo): "))
+    lugar_curso= incriptosActu + 1
     alumno = {
         "Nombre": nombre, "Apellido": apellido, "Nivel de Conocimiento": conocimiento,
-        "Nombre de Curso/Taller": curso
+        "Nombre de Curso/Taller": curso, "Lugar": lugar_curso,
     } #este es el armado del registro de cada inscripto
-    guardar_inscripcion(alumno)
+    
+    guardar_inscripcion(alumno, datos, arch)
 
-def guardar_inscripcion(alumno):
- arch= "inscriptos.json"
- try:
-     with open(arch,"r", encoding="utf-8") as archivo: #apertura del archivo en esc/lec
-         datos= json.load(archivo) #cargo datos
- except (FileNotFoundError, json.JSONDecodeError):
-         datos=[] #empiezo una lista nueva 
- datos.append(alumno)
- with open(arch, "w", encoding="utf-8") as archivo: #guardado del alumno
-        json.dump(datos, archivo, indent=4, ensure_ascii=False)
- print("¡Ya estás inscripto!")  
+
 
 menu = {
     "1": lambda: inscripcion("Curso de Inglés"),
@@ -54,6 +77,7 @@ accion = menu.get(opcion)
 if accion:
    accion()
 else:
-    print("Esa opcion no es valida. Intente nuevamente")     
+    print("Esa opcion no es valida. Intente nuevamente")  
+   
 
 
